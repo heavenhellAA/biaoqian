13ba9f4fef7f13eef4ccd3af761afde9f368040c
liujy <2152226758@qq.com>
2022-05-18 17:21:04 +0800

    11

--- BarCodePrintSys/APP_Code/Tools/Tools.cs
+++ BarCodePrintSys/APP_Code/Tools/Tools.cs
@@ -6,13 +6,58 @@ namespace BarCodePrintSys.APP_Code.Tools
 {
     public class Tools
     {
-        public string Liushui(int i , string tableName)
+        /// <summary>
+        /// 流水号生成工具
+        /// </summary>
+        /// <param name="i">记录在当批次序列的位置</param>
+        /// <param name="tableName">记录指向的表名称</param>
+        /// <param name="num">当批次总数</param>
+        /// <param name="columnName">序列字段名称</param>
+        /// <returns></returns>
+        public string LiushuiTool(int i, string tableName, int num, string columnName)
         {
-            string lsh = "";
-            string sql;
-            sql = null;
+            //流水规则：3位 批次数量 3位 在批次中的序列流水号 5位在库中的序列号
+            // 批次数量：本次打印的张数
+            // 批次中的序列流水号：循环的位置
+            string liushuihao;
+            string picishuliang;
+            if (num > 99999)
+            {
+                throw new Exception("超出长度！");
+            }
+            else
+            {
+                picishuliang = num.ToString().PadLeft(3, '0');
+            }
+            string picixuliehao;
+            if (i > num)
+            {
+                throw new Exception("超出本批次数量极限！");
+            }
+            else
+            {
+                picixuliehao = i.ToString().PadLeft(3, '0');
+            }
+            string dangrizaikuxulie;
+
+            //方案一：取当日时间段中最大的记录
+            string sql = "select  top 1 ";
+            sql += columnName + "from ";
+            sql += tableName;
+            sql += " where s_createtime  >= convert(varchar(10), Getdate(), 120)";
+            sql += " and s_createtime<convert(varchar(10),dateadd(d, 1, Getdate()),120)";
             DataSet ds = DBHelper.getDateSet(sql);
-            return lsh;
+
+            dangrizaikuxulie=ds.ToString();
+
+            liushuihao = picishuliang + picixuliehao + dangrizaikuxulie;
+
+            //====自动生成编号====//
+            //日期格式化
+
+            //取最新数据编号
+
+            return liushuihao;
         }
     }
 }
